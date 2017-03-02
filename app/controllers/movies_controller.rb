75e133f73c5b4ac8f18fb582de79ff50d97f39a7
David McKee <david0mckee@gmail.com>
2017-03-02 07:37:06 +0000

    Done with section 4.7, also fixed error where simple.css was actually supposed to be in application.css

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,10 +14,17 @@ class MoviesController < ApplicationController
   end
   
 
-def new
-  @movie = Movie.new
-  # default: render 'new' template
-end 
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end 
+  
+  def create
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
   
   private
   
